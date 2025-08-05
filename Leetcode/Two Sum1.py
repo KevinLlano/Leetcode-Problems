fb09e0dfa03dffa20d1f151d331e5578042dbfb0
Kevin Llanos <kllano2@wgu.edu>
2025-08-05 06:38:57 -0500

    adding first set up of problem

---
+++ Leetcode/Two Sum1.py
@@ -0,0 +1,24 @@
+# Better Brute Force Solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1, n):
+                if nums[i] + nums[j] == target:
+                    return (i, j)
+
+
+# 2-Pass Optimal Solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h = {}
+        for i in range(len(nums)):
+            h[nums[i]] = i
+
+        for i in range(len(nums)):
+            y = target - nums[i]
+
+            if y in h and h[y] != i:
+                return [i, h[y]]
+# Time Complexity: O(n)
+# Space Complexity: O(n)
