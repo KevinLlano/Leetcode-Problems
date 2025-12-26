8dedd395c1db8072b3fa30edc4ead0e034b3ae26
Kevin Llanos <kllano2@wgu.edu>
2025-12-25 22:27:56 -0600

    leetcode problem 121 and 125 solved

---
+++ Leetcode/121. Best Time to Buy and Sell Stock.java
@@ -0,0 +1,27 @@
+
+
+
+public class Solution {
+    // Returns the maximum profit from a single buy and sell transaction
+    public int maxProfit(int[] prices) {
+        // Initialize minPrice to the largest possible value
+        int minPrice = Integer.MAX_VALUE;
+        // Initialize maxProfit to 0
+        int maxProfit = 0;
+
+        // Iterate through each price in the array
+        for (int price : prices) {
+            // If current price is less than minPrice, update minPrice
+            if (price < minPrice) {
+                minPrice = price;
+            } 
+            // If selling at current price yields higher profit, update maxProfit
+            else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+
+        // Return the maximum profit found
+        return maxProfit;
+    }
+}
